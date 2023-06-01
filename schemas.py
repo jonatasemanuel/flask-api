e280962adea3351cd6856174dcc31b6c871f5c66
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-05-31 21:07:00 -0300

    sqlalchemy models

--- schemas.py
+++ schemas.py
@@ -1,18 +1,26 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
 
 
-class ItemUpdateSchema(Schema):
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class PlainItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(lambda: ItemSchema()), dump_only=True)
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(lambda: StoreSchema(), dump_only=True)
